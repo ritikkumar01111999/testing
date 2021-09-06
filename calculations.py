0a2b3f3fca9e6ad3a504282fbf8bdb602073ae2e
RITIK KUMAR <89376306+ritikkumar01111999@users.noreply.github.com>
2021-09-06 10:48:16 +0530

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -1,17 +1,22 @@
 
 
-def sum(number1,number2):
-    result= number1+number2
-    return result
+def sum(number1,number2): #two arguments passed in the function
+    #function to accept two numbers for adding
+    result= number1+number2 #adding two number
+    return result # returnig result to function
 
-def sub(number1,number2):
-    result= number1-number2
-    return result
-def mul(number1,number2):
-    result= number1*number2
-    return result
+def sub(number1,number2): #two arguments passed in the function
+    #function to accept two numbers for substracting
+    result= number1-number2 #substracting two number
+    return result  # returnig result to function
 
-def div(number1,number2):
-    result= number1/number2
-    return result
+def mul(number1,number2): #two arguments passed in the function
+    #function to accept two numbers for multiplying
+    result= number1*number2 #multiplying two number
+    return result  # returnig result to function
+
+def div(number1,number2): #two arguments passed in the function
+    #function to accept two numbers fro divide
+    result= number1/number2 # divide two number
+    return result  # returnig result to function
 
