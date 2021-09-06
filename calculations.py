d00bd442cbd5ae1674b70d0d904d5609d8b0fae1
RITIK KUMAR <89376306+ritikkumar01111999@users.noreply.github.com>
2021-09-06 10:41:19 +0530

    Add files via upload

---
+++ calculations.py
@@ -0,0 +1,17 @@
+
+
+def sum(number1,number2):
+    result= number1+number2
+    return result
+
+def sub(number1,number2):
+    result= number1-number2
+    return result
+def mul(number1,number2):
+    result= number1*number2
+    return result
+
+def div(number1,number2):
+    result= number1/number2
+    return result
+
